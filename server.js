35ef47470d630d48632afd8436d887f8ad72d76c
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-07-02 14:03:21 -0400

    Configura estrutura inicial do projeto e adiciona arquivos principais

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-import { Analytics } from "@vercel/analytics/next"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
